3dcc9bb5c1de0230d16e67e6b934139518a895c0
IgnaDev <arcortoon@gmail.com>
2023-02-01 18:42:08 -0300

    feat: :fire: Cree nuestro primeros componente

--- veterinario-citas/src/App.jsx
+++ veterinario-citas/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
   const real = false
   return (
     <div className="App">
       <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
       <h2>{real ? "Real no fake" : "No real, totalmente fake"}</h2>
     </div>
   )
---
+++ veterinario-citas/src/components/ListadoPacientes.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListadoPacientes() {
+  return (
+    <h2>Listado Pacientes</h2>
+  )
+}
