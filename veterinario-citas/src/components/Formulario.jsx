a14299a1458bef7188333b8248bcbffea10614c1
IgnaDev <arcortoon@gmail.com>
2023-02-03 20:24:53 -0300

    feat: :sparkles: Estilos del formularios listos, me gusto tailwind

--- veterinario-citas/src/components/Formulario.jsx
+++ veterinario-citas/src/components/Formulario.jsx
@@ -6,11 +6,37 @@ export default function Formulario() {
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">administralos</span>
       </p>
-      <form action="" className="bg-white shadow-md rounded-md px-5 py-10">
-        <div>
+
+      <form action="" className="bg-white shadow-md rounded-md px-5 py-10 mb-10">
+        <div className="mb-5">
           <label className="block text-gray-700 uppercase font-bold" htmlFor="mascota">Nombre Mascota</label>
           <input id="mascota" className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md" type="text" placeholder="Nombre de la mascota" />
         </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="Propietario">Nombre Propietario</label>
+          <input id="Propietario" className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md" type="text" placeholder="Nombre de la mascota" />
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="Email">Email</label>
+          <input id="Email" className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md" type="text" placeholder="Email del propietario" />
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="Alta">Alta</label>
+          <input id="Alta" className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md" type="date" />
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="Sintomas">Sintomas</label>
+          <textarea name="" id="Sintomas" className="border-2 w-full p-2 mt-2" placeholder="Sintomas"></textarea>
+        </div>
+
+        <input 
+        type="submit" 
+        className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all" 
+        value="Agregar Paciente" />
       </form>
     </div>
   )
