aaeb648a50cc21b5306cf8f38f6e42ce8656beeb
IgnaDev <arcortoon@gmail.com>
2023-02-03 21:12:18 -0300

    feat: :sparkles: Estilos y componente paciente

--- veterinario-citas/src/components/ListadoPacientes.jsx
+++ veterinario-citas/src/components/ListadoPacientes.jsx
@@ -1,7 +1,17 @@
+import Paciente from "./Paciente"
+
 export default function ListadoPacientes() {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold">Pacientes y citas</span>
+      </p>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
     </div>
   )
 }
---
+++ veterinario-citas/src/components/Paciente.jsx
@@ -0,0 +1,21 @@
+export default function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre Propietario: {''}
+            <span className="font-normal normal-case">Diggy</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+            <span className="font-normal normal-case">hook@react.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+            <span className="font-normal normal-case">5/05/2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+            <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex voluptas assumenda autem! Quo nihil amet reiciendis sapiente aliquam blanditiis dignissimos sit libero odio. Officiis, fugit provident tempore tempora quos ipsum.</span>
+        </p>
+    </div>
+  )
+}
