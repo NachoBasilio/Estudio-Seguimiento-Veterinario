a291b28be568fce8bc6b5ef881c981b2f28f381f
IgnaDev <arcortoon@gmail.com>
2023-02-08 17:00:13 -0300

    feat: aplicado localStorage

--- veterinario-citas/src/App.jsx
+++ veterinario-citas/src/App.jsx
@@ -1,11 +1,32 @@
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
+
+  useEffect(()=>{
+    const obtenerLS = ()=>{
+      if(JSON.parse(localStorage.getItem('pacientes'))!== null){
+        const pacientesLS = JSON.parse(localStorage.getItem('pacientes'))
+        setPacientes(pacientesLS) 
+        
+      }else{
+        const pacientesLS =[]
+        setPacientes(pacientesLS)
+      }
+
+
+    }
+
+    obtenerLS()
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  },[pacientes])
 
   const eliminarPaciente = (id)=>{
     const pacientesActualizados = pacientes.filter(pacienteState => pacienteState.id !== id)
